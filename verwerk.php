c61283578b3cdf387dd5c4f575cfc657cece9c3b
Peter-The-Great <PjotrW15@Gmail.com>
2022-01-12 19:45:26 +0100

    gelukt om files te uploaden

--- verwerk.php
+++ verwerk.php
@@ -6,16 +6,17 @@ use Composer\Console\Application;
 // instantiate the actual parser
 // and parse them from a given file, this could be any file or a posted string
 //__DIR__.
+$Toegestaan = array("txt","swi","mt940");
+$fileExt = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 
-if(isset($_FILES['file'])){
+if(isset($_FILES['file']) && in_array($fileExt,$Toegestaan)){
     $parser = new \Kingsquare\Parser\Banking\Mt940();
     $engine = new \Kingsquare\Parser\Banking\Mt940\Engine\Rabo();
     $tmpFile = $_FILES['file'];
     $Tijdelijk = $tmpFile['tmp_name'];
     $name = $tmpFile['name'];
-    $type = $tmpFile['type'];
-    $parsedStatements = $parser->parse(file_get_contents($tmpFile), $engine);
-    //print_r($parsedStatements);
+    var_dump($tmpFile);
+    $parsedStatements = $parser->parse(file_get_contents($Tijdelijk), $engine);
 
     $parsedStatements = json_encode($parsedStatements);
 
